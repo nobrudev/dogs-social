93e5ec073183160074b49457f64e41fe1426f041
Bruno Alves <brunoadev@gmail.com>
2026-04-13 14:48:59 -0300

    Primeiro commit: Levanto tudo para o hub

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../Hooks/useForm";
+import { TOKEN_POST, USE_GET } from "../../api";
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USE_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault(); /*previne o padrao para que nao recarregue a pagina toda vez que clicar*/
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
