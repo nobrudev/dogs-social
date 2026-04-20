ef56c32a4b7cc6b32361a42953946cbebfc83ce7
Bruno Alves <brunoadev@gmail.com>
2026-04-20 12:24:39 -0300

    aula Feed Modal dois

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
 
 const PhotoContent = ({ data }) => {
-  const { phot, comments } = data;
+  const { photo, comments } = data;
   return (
     <div className={styles.photo}>
-      <div className={StyleSheet.img}>
+      <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
-            <h1 className="title">
-              <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-            </h1>
-            <ul className={styles.attributes}>
-              <li>{photo.peso} kg</li>
-              <li>{photo.idade} anos</li>
-            </ul>
           </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
         </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />
