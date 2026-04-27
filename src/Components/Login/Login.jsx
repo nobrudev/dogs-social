a743bbc8ce7a17ce46a2381cd2d8921650af1689
Bruno Alves <brunoadev@gmail.com>
2026-04-27 12:13:36 -0300

    aula perde senha um

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="criar" element={<LoginCreate />} />
-          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="esqueceu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
